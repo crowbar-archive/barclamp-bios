8552901d9e931b0f6fb9e8931763047c87670c7f
Raajeev <Raajeev_Kalyanaraman@dell.com>
2013-07-03 05:35:10 -0500

    Sync of recipes with Hadoop 2.3

--- chef/cookbooks/bios/recipes/bios-tools.rb
+++ chef/cookbooks/bios/recipes/bios-tools.rb
@@ -1,6 +1,17 @@
+#!/usr/bin/ruby
+# Copyright (c) 2013 Dell Inc.
 #
-# Copyright (c) 2011 Dell Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 #  Ensure that tools required to work with BIOS are installed.
 # (Applies both to PEC and WSMAN variants)
@@ -10,8 +21,8 @@ include_recipe "bios::bios-common"
 
 provisioner_server = (node[:crowbar_wall][:provisioner_server] rescue nil)
 return unless provisioner_server
-bmc="bmc-2012-10-17.tgz"
-setupbios="setupbios-2012-10-10.tgz"
+bmc="bmc-2013-02-11.tgz"
+setupbios="setupbios-2013-03-05.tgz"
 socflash="socflash_v10601.zip"
 
 
